5825fcdab3a15f0ddf84dd1387dd465d193541a3
Christo <christopher.pkm@gmail.com>
2026-06-11 11:21:31 +0100

    correctly renamed a ui test

--- e2e/boardResets.spec.ts
+++ e2e/boardResets.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("test", async ({ page }) => {
+test("board resets", async ({ page }) => {
     await page.goto("http://localhost:3000/");
     await page.locator("div:nth-child(3)").first().click();
     await page.locator("div:nth-child(2) > div:nth-child(3)").first().click();
